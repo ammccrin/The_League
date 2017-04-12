e8d2fd43d260dbf9e1790b35d31871f6734279c5
Andrew McCrink <mccrinkandrew@gmail.com>
2017-04-12 12:24:08 -0400

    Delete 2 repetitive migrations

--- db/migrate/20170407215018_weeks.rb
+++
@@ -1,9 +0,0 @@
-class Weeks < ActiveRecord::Migration[5.0]
-  def change
-  	create_table :weeks do |t|
-    	t.integer :week, null: false
-    	t.references :league, null: false
-      t.timestamps
-    end
-  end
-end
